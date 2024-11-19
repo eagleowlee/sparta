7e53db67c70d1fe5ac0b71ebba64c5940f966ce9
eagleowlee <betterjunior@naver.com>
2024-11-19 20:23:38 +0900

    Add files via upload

---
+++ Calcucu.java
@@ -0,0 +1,24 @@
+package CACACA;
+
+public class Calcucu {
+    public double calculate(String option, int num1, int num2) {
+
+        double answer = 0;
+
+        if (option.equals("+")) {
+            answer = num1 + num2;
+        } else if (option.equals("-")) {
+            answer = num1 - num2;
+        } else if (option.equals("*")) {
+            answer = num1 * num2;
+        } else if (option.equals("/")) {
+            if (num2 != 0) {
+                answer = num1 / num2;
+            } else {
+                return 0;
+            }
+        }
+            return answer;
+        }
+    }
+
